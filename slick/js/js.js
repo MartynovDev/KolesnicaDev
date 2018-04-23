a9fcc23b7b62d42035f4a2fd9b3cdf40149e1314
Denis <denis.dev.mart17@gmail.com>
2018-04-23 21:41:07 +0300

    add project

---
+++ slick/js/js.js
@@ -0,0 +1,40 @@
+$('.sl').slick({
+/*    autoplay: true,
+    autoplaySpeed: 10000,*/
+    cssEase: 'ease-in',
+    asNavFor: '.sl2',
+    arrows: false
+});
+$('.sl2').slick({
+    slidesToShow: 3,
+    dots: true,
+    slidesToScroll: 1,
+    asNavFor: '.sl',
+    centerPadding:'40px',
+    centerMode: true,
+    focusOnSelect: true,
+    arrows: false,
+    responsive:[
+        {
+            breakpoint:900,
+            settings:{
+             slidesToShow: 3
+        }
+    }
+]
+});
+
+
+a='e';
+i=arr[].size();
+arr[] = {'a', 'b', 'c','d', 'e', 'y', 'o'};
+
+whie(arr[i]!=a){
+    if(i>)
+}
+
+
+/*for(int i = 0; i<arr.size(); i++){
+    if()
+}*/
+		
